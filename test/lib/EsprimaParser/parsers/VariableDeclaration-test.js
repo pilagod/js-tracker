dfe8d35f8cc4c3d94f414752cb3b424aa3b94aec
pilagod <hotony801018@gmail.com>
2016-07-06 17:01:15 +0800

    parser VariableDeclaration added

---
+++ test/lib/EsprimaParser/parsers/VariableDeclaration-test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// spec: https://github.com/estree/estree/blob/master/spec.md#variabledeclaration
+
+describe('VariableDeclaration tests', () => {
+  let variableDeclaration
+
+  beforeEach(() => {
+    variableDeclaration = createAstNode('VariableDeclaration')
+
+    sandbox.stub(esprimaParser, 'parseNode', sandbox.spy())
+  })
+
+  it('should call parseNode on nodes in declarations', () => {
+    variableDeclaration.declarations = [
+      createAstNode('VariableDeclarator'),
+      createAstNode('VariableDeclarator')
+    ]
+
+    esprimaParser.VariableDeclaration(variableDeclaration)
+
+    variableDeclaration.declarations.forEach((node, index) => {
+      expect(
+        esprimaParser.parseNode
+          .getCall(index)
+          .calledWithExactly(node)
+      ).to.be.true
+    })
+  })
+})
