82c955a6f98778d9c7bdc848da45573afbd7e34f
pilagod <hotony801018@gmail.com>
2016-07-06 16:29:18 +0800

    change test text

--- test/lib/EsprimaParser/parsers/Literal-test.js
+++ test/lib/EsprimaParser/parsers/Literal-test.js
@@ -9,37 +9,37 @@ describe('Literal tests', () => {
     literal = createAstNode('Literal')
   })
 
-  it('should return number given number \'value\'', () => {
-    literal.value = 1
-
-    expect(esprimaParser.Literal(literal)).to.be.equal(1)
-  })
-
-  it('should return string given string \'value\'', () => {
+  it('should return \'string\' given value \'string\'', () => {
     literal.value = 'string'
 
     expect(esprimaParser.Literal(literal)).to.be.equal('string')
   })
 
-  it('should return boolean given boolean \'value\'', () => {
+  it('should return 1 given value 1', () => {
+    literal.value = 1
+
+    expect(esprimaParser.Literal(literal)).to.be.equal(1)
+  })
+
+  it('should return true given value true', () => {
     literal.value = true
 
     expect(esprimaParser.Literal(literal)).to.be.equal(true)
   })
 
-  it('should return null given null \'value\'', () => {
+  it('should return null given value null', () => {
     literal.value = null
 
     expect(esprimaParser.Literal(literal)).to.be.equal(null)
   })
 
-  it('should return regexp given \'regex\'', () => {
+  it('should return regexp given regex', () => {
     literal.regex = {
       pattern: '\\w+',
       flags: 'g'
     }
 
-    let result = esprimaParser.Literal(literal)
+    const result = esprimaParser.Literal(literal)
 
     expect(result).to.be.instanceof(RegExp)
     expect('this is a string'.match(result)).to.be.eql(['this', 'is', 'a', 'string'])
