2e3f98ad6900ae8d1e9dfdd7f7d64f9fcaa9954b
pilagod <hotony801018@gmail.com>
2016-07-11 16:25:47 +0800

    transformUpdateToAssignment added

---
+++ test/lib/EsprimaParser/parsers/transformUpdateToAssignment-test.js
@@ -0,0 +1,20 @@
+'use strict'
+
+describe('transformUpdateToAssignment tests', () => {
+  for (const operator of [['++', '+='], ['--', '-=']]) {
+    it(`should return AssignmentExpression \'${operator[1]}\' format from UpdateExpression \'${operator[0]}\'`, () => {
+      const updateExpression = createAstNode('UpdateExpression', {
+        operator: operator[0],
+        argument: 'left'
+      })
+
+      const result = esprimaParser.transformUpdateToAssignment(updateExpression)
+
+      expect(result).to.be.eql(createAstNode('AssignmentExpression', {
+        operator: operator[1],
+        left: 'left',
+        right: createAstNode('Literal', {value: 1, raw: '1'})
+      }))
+    })
+  }
+})
