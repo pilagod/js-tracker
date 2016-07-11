fb644be391716ddb40df58da67e197b22c316741
pilagod <hotony801018@gmail.com>
2016-07-11 19:05:11 +0800

    SequenceExpression added

---
+++ test/lib/EsprimaParser/parsers/SequenceExpression-test.js
@@ -0,0 +1,35 @@
+// spec: https://github.com/estree/estree/blob/master/spec.md#sequenceexpression
+
+describe('SequenceExpression tests', () => {
+  let sequenceExpression
+
+  beforeEach(() => {
+    sequenceExpression = createAstNode('SequenceExpression', {
+      expressions: [
+        createAstNode('Literal', {value: 'first'}),
+        createAstNode('Literal', {value: 'second'}),
+        createAstNode('Literal', {value: 'third'})
+      ]
+    })
+
+    sandbox.stub(esprimaParser, 'parseNode', sandbox.spy(createLiteralStub()))
+  })
+
+  it('should call parseNode with each expression', () => {
+    esprimaParser.SequenceExpression(sequenceExpression)
+
+    sequenceExpression.expressions.forEach((expression, index) => {
+      expect(
+        esprimaParser.parseNode
+          .getCall(index)
+          .calledWithExactly(expression)
+      ).to.be.true
+    })
+  })
+
+  it('should return last expression result', () => {
+    const result = esprimaParser.SequenceExpression(sequenceExpression)
+
+    expect(result).to.be.equal('third')
+  })
+})
