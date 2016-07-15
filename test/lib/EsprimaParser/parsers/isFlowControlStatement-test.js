d966825b663cb0e596b956c41022672d10b7b817
pilagod <hotony801018@gmail.com>
2016-07-15 10:48:04 +0800

    isFlowControlStatement added

---
+++ test/lib/EsprimaParser/parsers/isFlowControlStatement-test.js
@@ -0,0 +1,25 @@
+describe('isFlowControlStatement', () => {
+  it('should return true given type ReturnStatement', () => {
+    const result = esprimaParser.isFlowControlStatement('ReturnStatement')
+
+    expect(result).to.be.true
+  })
+
+  it('should return true given type ContinueStatement', () => {
+    const result = esprimaParser.isFlowControlStatement('ContinueStatement')
+
+    expect(result).to.be.true
+  })
+
+  it('should return true given type BreakStatement', () => {
+    const result = esprimaParser.isFlowControlStatement('BreakStatement')
+
+    expect(result).to.be.true
+  })
+
+  it('should return false given other type of flow control statement', () => {
+    const result = esprimaParser.isFlowControlStatement('NotFlowControlStatement')
+
+    expect(result).to.be.false
+  })
+})
