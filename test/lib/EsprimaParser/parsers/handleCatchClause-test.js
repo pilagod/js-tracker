d83c05f0567dd7d72def09318a6006365c734d88
pilagod <hotony801018@gmail.com>
2016-07-24 12:07:02 +0800

    handleCatchClause added

--- test/lib/EsprimaParser/parsers/handleCatchClause-test.js
+++ test/lib/EsprimaParser/parsers/handleCatchClause-test.js
@@ -1,14 +1,58 @@
-// it('should not call handleCatchClause and throw error got from block given null handler', () => {
-//   tryStatement.handler = null
-//
-//   esprimaParser.parseNode
-//     .withArgs(tryStatement.block)
-//       .throws(error)
-//
-//   try {
-//     esprimaParser.TryStatement(tryStatement)
-//   } catch (e) {
-//     expect(e).to.be.eql(error)
-//   }
-//   expect(esprimaParser.handleCatchClause.called).to.be.false
-// })
+describe('handleCatchClause tests', () => {
+  const error = new Error('error from block')
+  let handler
+
+  beforeEach(() => {
+    handler = createAstNode('CatchClause', {
+      param: createAstNode('Pattern'),
+      body: createAstNode('BlockStatement')
+    })
+  })
+
+  describe('given non-null handler', () => {
+    beforeEach(() => {
+      sandbox.stub(esprimaParser, 'getNameFromPattern')
+        .returns('resultFromGetNameFromPattern')
+      sandbox.stub(esprimaParser, 'setVariables')
+      sandbox.stub(esprimaParser, 'parseNode', createParseNodeStub())
+    })
+
+    it('should call getNameFromPattern with handler param', () => {
+      esprimaParser.handleCatchClause(handler, error)
+
+      expect(
+        esprimaParser.getNameFromPattern
+          .calledWithExactly(handler.param)
+      ).to.be.true
+    })
+
+    it('should call setVariables with result from getNameFromPattern and error', () => {
+      esprimaParser.handleCatchClause(handler, error)
+
+      expect(
+        esprimaParser.setVariables
+          .calledWithExactly('resultFromGetNameFromPattern', error)
+      ).to.be.true
+    })
+
+    it('should call parseNode with handler body and return', () => {
+      const result = esprimaParser.handleCatchClause(handler, error)
+
+      expect(
+        esprimaParser.parseNode
+          .calledWithExactly(handler.body)
+      ).to.be.true
+      expect(result).to.be.equal('parsedBlockStatement')
+    })
+  })
+
+  describe('given null handler', () => {
+    it('should throw error got from block', () => {
+      handler = null
+
+      expect(
+        esprimaParser.handleCatchClause.bind(esprimaParser, handler, error)
+      ).to.throw(error)
+    })
+  })
+})
