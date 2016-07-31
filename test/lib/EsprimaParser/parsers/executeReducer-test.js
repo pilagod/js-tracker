62e99acf764920148be6bb01ba916934db3820a9
pilagod <hotony801018@gmail.com>
2016-07-31 22:21:05 +0800

    executeReducer added

--- test/lib/EsprimaParser/parsers/executeReducer-test.js
+++ test/lib/EsprimaParser/parsers/executeReducer-test.js
@@ -1,2 +1,34 @@
 describe('executeReducer tests', () => {
+  const pre = 'pre'
+  const method = 'method'
+
+  beforeEach(() => {
+    sandbox.stub(esprimaParser, 'Method', function () {})
+  })
+
+  it('should call executeCall with pre and cur given cur is instance of Method', () => {
+    const cur = new (esprimaParser.Method)(method)
+
+    sandbox.stub(esprimaParser, 'executeCall')
+
+    esprimaParser.executeReducer(pre, cur)
+
+    expect(
+      esprimaParser.executeCall
+        .calledWithExactly(pre, cur)
+    ).to.be.true
+  })
+
+  it('should call executeMember with pre and cur given cur is not instance of Method', () => {
+    const cur = 'cur'
+
+    sandbox.stub(esprimaParser, 'executeMember')
+
+    esprimaParser.executeReducer(pre, cur)
+
+    expect(
+      esprimaParser.executeMember
+        .calledWithExactly(pre, cur)
+    ).to.be.true
+  })
 })
