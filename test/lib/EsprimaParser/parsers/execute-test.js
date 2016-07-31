4e49bd1706744bffb49cfce01476f5c65c4d3e3b
pilagod <hotony801018@gmail.com>
2016-07-31 15:12:26 +0800

    execute added

--- test/lib/EsprimaParser/parsers/execute-test.js
+++ test/lib/EsprimaParser/parsers/execute-test.js
@@ -1,3 +1,34 @@
 describe('execute tests', () => {
-  
+  const data = ['data']
+
+  beforeEach(() => {
+    sandbox.stub(esprimaParser, 'executeExpression')
+  })
+
+  it('should call executeExpression with data', () => {
+    esprimaParser.execute(data)
+
+    expect(
+      esprimaParser.executeExpression
+        .calledWithExactly(data)
+    ).to.be.true
+  })
+
+  it('should return result from executeExpression given no error threw', () => {
+    esprimaParser.executeExpression
+      .returns('resultFromExecuteExpression')
+
+    const result = esprimaParser.execute(data)
+
+    expect(result).to.be.equal('resultFromExecuteExpression')
+  })
+
+  it('should return undefined given error threw from executeExpression', () => {
+    esprimaParser.executeExpression
+      .throws(new Error('errorFromExecuteExpression'))
+
+    const result = esprimaParser.execute(data)
+
+    expect(result).to.be.undefined
+  })
 })
