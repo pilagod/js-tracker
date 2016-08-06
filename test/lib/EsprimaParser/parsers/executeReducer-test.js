d1f1516ed2e2680c6c1cc29485d2f2823565a041
pilagod <hotony801018@gmail.com>
2016-08-06 18:52:10 +0800

    executeReducer refined

--- test/lib/EsprimaParser/parsers/executeReducer-test.js
+++ test/lib/EsprimaParser/parsers/executeReducer-test.js
@@ -6,29 +6,33 @@ describe('executeReducer tests', () => {
     sandbox.stub(esprimaParser, 'Method', function () {})
   })
 
-  it('should call executeCall with pre and cur given cur is instance of Method', () => {
+  it('should call executeCall with pre and cur given cur is instance of Method and return', () => {
     const cur = new (esprimaParser.Method)(method)
 
     sandbox.stub(esprimaParser, 'executeCall')
+      .returns('resultFromExecuteCall')
 
-    esprimaParser.executeReducer(pre, cur)
+    const result = esprimaParser.executeReducer(pre, cur)
 
     expect(
       esprimaParser.executeCall
         .calledWithExactly(pre, cur)
     ).to.be.true
+    expect(result).to.be.equal('resultFromExecuteCall')
   })
 
-  it('should call executeMember with pre and cur given cur is not instance of Method', () => {
+  it('should call executeMember with pre and cur given cur is not instance of Method and return', () => {
     const cur = 'cur'
 
     sandbox.stub(esprimaParser, 'executeMember')
+      .returns('resultFromExecuteMember')
 
-    esprimaParser.executeReducer(pre, cur)
+    const result = esprimaParser.executeReducer(pre, cur)
 
     expect(
       esprimaParser.executeMember
         .calledWithExactly(pre, cur)
     ).to.be.true
+    expect(result).to.be.equal('resultFromExecuteMember')
   })
 })
