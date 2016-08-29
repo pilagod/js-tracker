c156338c28b80716ddf15b4a7723b817c2ae099a
pilagod <hotony801018@gmail.com>
2016-08-29 18:06:20 +0800

    createPropDispatcher added

---
+++ test/lib/EsprimaParser-unitests/dispatchers/createPropDispatcher-test.js
@@ -0,0 +1,42 @@
+describe('createPropDispatcher tests', () => {
+  let Dispatcher, createPropDispatcher, info, callDispatcher
+
+  before(() => {
+    Dispatcher = require(`${libDir}/structures/Dispatcher`)
+    createPropDispatcher = require(`${libDir}/dispatchers/createPropDispatcher`)
+
+    info = {
+      path: `${__dirname}/${libDir}/dispatchers`,
+      options: {dir: false}
+    }
+    propDispatcher = createPropDispatcher(info)
+  })
+
+  it('should return a Dispatcher instance', () => {
+    expect(propDispatcher).to.be.instanceof(Dispatcher)
+  })
+
+  it('should import all handlers to Dispatcher from given path and options', () => {
+    const handlers = importAllFrom(info.path, info.options)
+
+    expect(propDispatcher.handlers).to.be.eql(handlers)
+  })
+
+  it('should return true when test called with data whose callee is typeof string', () => {
+    const data = {
+      callee: 'prop'
+    }
+    const result = propDispatcher.test(data)
+
+    expect(result).to.be.true
+  })
+
+  it('should return true when test called with data whose callee is not typeof string', () => {
+    const data = {
+      callee: {}
+    }
+    const result = propDispatcher.test(data)
+
+    expect(result).to.be.false
+  })
+})
