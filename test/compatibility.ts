57eaab456e329d3f728cd97bdf7c0057cf84d39e
pilagod <pilagooood@gmail.com>
2017-05-22 22:28:22 +0800

    add NamedNodeMap NS compatibility test

--- test/compatibility.ts
+++ test/compatibility.ts
@@ -16,6 +16,7 @@ describe('compatibility of default behaviors', function () {
     }
     div.click()
   })
+
   it('should compatible with Element default behaviors', () => {
     const div = document.createElement('div')
 
@@ -33,6 +34,7 @@ describe('compatibility of default behaviors', function () {
     div.setAttribute('id', 'id2')
     expect(div.id).to.equal('id2')
   })
+
   it('should compatible with Node default behaviors', () => {
     const div = document.createElement('div')
 
@@ -46,6 +48,7 @@ describe('compatibility of default behaviors', function () {
     expect(div.children.length).to.equal(1)
     expect(div.children[0]).to.equal(div2)
   })
+
   it('should compatible with EventTarget default behaviors', (done) => {
     const div = document.createElement('div')
 
@@ -55,6 +58,7 @@ describe('compatibility of default behaviors', function () {
     })
     div.click()
   })
+
   it('should compatible with Attr default behaviors', () => {
     const div = document.createElement('div')
 
@@ -62,6 +66,7 @@ describe('compatibility of default behaviors', function () {
     div.getAttributeNode('id').value = 'id2'
     expect(div.id).to.equal('id2')
   })
+
   it('should compatible with CSSStyleDeclaration default behaviors', () => {
     const div = document.createElement('div')
 
@@ -73,12 +78,14 @@ describe('compatibility of default behaviors', function () {
     div.style.setProperty('background-color', 'red')
     expect(div.style.backgroundColor).to.equal('red')
   })
+
   it('should compatible with DOMStringMap default behaviors', () => {
     const div = document.createElement('div')
 
     div.dataset.data = 'data'
     expect(div.dataset.data).to.equal('data')
   })
+
   it('should compatible with DOMTokenList default behaviors', () => {
     const div = document.createElement('div');
 
@@ -90,14 +97,22 @@ describe('compatibility of default behaviors', function () {
     div.classList.add('class2')
     expect(div.classList.toString()).to.equal('class1 class2')
   })
+
   it('should compatible with NamedNodeMap default behaviors', () => {
     const div = document.createElement('div')
     const attrid = document.createAttribute('id')
 
     attrid.value = 'id'
 
-    /* method test - complex */
     div.attributes.setNamedItem(attrid)
-    expect(div.id).to.equal('id')
+    expect(div.getAttribute('id')).to.equal('id')
+
+    const divNS = document.createElement('div')
+    const attridNS = document.createAttributeNS('ns', 'id')
+
+    attridNS.value = 'idns'
+
+    divNS.attributes.setNamedItemNS(attridNS)
+    expect(divNS.getAttributeNS('ns', 'id')).to.equal('idns')
   })
 })
