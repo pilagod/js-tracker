d499951c96ddf9b128837a63118466fb715839f1
pilagod <pilagooood@gmail.com>
2017-06-06 20:08:30 +0800

    change ActionTagMap presentation

--- src/tracker/ActionTagMap.ts
+++ src/tracker/ActionTagMap.ts
@@ -1,29 +1,29 @@
 const ActionTagMap: object = {
   'Element': {
-    'removeAttribute': '#arg.0',
-    'removeAttributeNode': '#arg.0.value',
-    'removeAttributeNS': '#arg.1',
-    'setAttribute': '#arg.0',
-    'setAttributeNode': '#arg.0.value',
-    'setAttributeNodeNS': '#arg.0.value',
-    'setAttributeNS': '#arg.1',
+    'removeAttribute': ['#arg', '0'],
+    'removeAttributeNode': ['#arg', '0', 'name'],
+    'removeAttributeNS': ['#arg', '1'],
+    'setAttribute': ['#arg', '0'],
+    'setAttributeNode': ['#arg', '0', 'name'],
+    'setAttributeNodeNS': ['#arg', '0', 'name'],
+    'setAttributeNS': ['#arg', '1'],
   },
   'Attr': {
-    'value': '#name'
+    'value': ['#name']
   },
   'DOMTokenList': {
-    'value': '#which',
+    'value': ['#which'],
 
-    'add': '#which',
-    'remove': '#which',
-    'replace': '#which',
-    'toggle': '#which'
+    'add': ['#which'],
+    'remove': ['#which'],
+    'replace': ['#which'],
+    'toggle': ['#which']
   },
   'NamedNodeMap': {
-    'removeNamedItem': '#arg.0',
-    'removeNamedItemNS': '#arg.1',
-    'setNamedItem': '#arg.0',
-    'setNamedItemNS': '#arg.1',
+    'removeNamedItem': ['#arg', '0'],
+    'removeNamedItemNS': ['#arg', '1'],
+    'setNamedItem': ['#arg', '0', 'name'],
+    'setNamedItemNS': ['#arg', '0', 'name'],
   }
 }
 export default ActionTagMap
