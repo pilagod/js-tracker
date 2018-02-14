210601bb66886ce1cdfa791a4f8f168660ebbe31
pilagod <pilagooood@gmail.com>
2018-02-14 12:26:55 +0800

    [refine] example updated

--- example/script2.js
+++
@@ -1,9 +0,0 @@
-var button = $('button')
-
-button.on('mouseenter', function () {
-  $(this).addClass('hover');
-});
-
-button.on('mouseleave', function (event) {
-  event.target.classList.remove('hover');
-});
