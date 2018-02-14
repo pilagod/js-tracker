cca2905716ea3c837fa56c35bb151463e4b4dd4f
pilagod <pilagooood@gmail.com>
2018-02-14 12:28:03 +0800

    [refine] remove example page (move it to gh-pages)

--- example/script1.js
+++
@@ -1,9 +0,0 @@
-var button = document.getElementsByTagName('button')[0];
-
-button.addEventListener('click', function () {
-  if (this.innerHTML === 'unclicked') {
-    this.innerHTML = 'clicked';
-  } else {
-    this.innerHTML = 'unclicked';
-  }
-});
